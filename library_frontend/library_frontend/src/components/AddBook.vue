8c3ad3b178323174d1949f11f6f7cdf8358e5815
mareknowak98 <mark-now@wp.pl>
2021-08-30 00:14:52 +0200

    adding book

--- library_frontend/library_frontend/src/components/AddBook.vue
+++ library_frontend/library_frontend/src/components/AddBook.vue
@@ -2,6 +2,8 @@
   <div class="container">
   <navbar></navbar>
   <b-jumbotron class="jumbotron jumbotron-home">
+    <h2>Authors</h2>
+    {{authors}}
     <h1>Add book:</h1>
       <b-form @submit.prevent="registerUser">
       <b-form-group id="input-group-1" label="Book name:" label-for="input-1">
@@ -12,6 +14,16 @@
           placeholder="Enter username"
         ></b-form-input>
         </b-form-group>
+
+        <b-form-group id="input-group-2" label="Author id (temporary):" label-for="input-2">
+        <b-form-input
+          id="input-2"
+          v-model="form.authorId"
+          required
+          placeholder="Enter author's id"
+        ></b-form-input>
+        </b-form-group>
+
       <b-button block type="submit" variant="secondary">Submit</b-button>
       </b-form>
     <Footer></Footer>
@@ -39,11 +51,15 @@ import axios from 'axios';
       return {
         form: {
           name: '',
-        }
+          authorId: ''
+        },
+        authors: {},
+        book: '',
       }
     },
 
-    mounted: function () {
+    mounted() {
+        this.getAuthors();
     },
 
     computed: {
@@ -51,23 +67,58 @@ import axios from 'axios';
     },
 
     methods:{
-      registerUser(){
-        console.log('es')
-        let config = {
+    //   registerUser(){
+    //     console.log('es')
+    //     let config = {
+    //       headers: {
+    //         'Content-Type': 'application/json',
+    //         "accept": "*/*"
+    //       }
+    //     }
+    //       axios.post('http://localhost:5000/api/books',{
+    //       name: this.form.name,
+    //       }, config)
+    //       .catch(err => {
+    //       console.log(err);
+    //       })
+    //     console.log('es2')
+    //   }
+
+
+    registerUser(){
+        axios({
+            url: 'http://localhost:5000/books',
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json', 
+                // 'Authorization': 'Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiIxIiwiaWF0IjoxNjMwMjM4MTYyLCJleHAiOjE2MzA4NDI5NjJ9.OWH9qtkiquie7Vhl0cz05vRqdY1MSufRrWnJJzQnEJi_e4X0QjLECqIdJ2P2drsjw38Zgxzz61qb2NShNAv3CA'
+                },
+            data: {
+                name: this.form.name
+            }
+        }).then(resp => (this.book = resp.data.id ,
+        axios({
+            url: 'http://localhost:5000/books/' + resp.data.id + '/authors/' + this.form.authorId + '/add',
+            method: 'post',
+            headers: {
+                'Content-Type': 'application/json', 
+                },
+        })))
+
+      },
+
+      getAuthors(){
+         let config = {
           headers: {
             'Content-Type': 'application/json',
-            "accept": "*/*"
           }
         }
-          axios.post('http://localhost:5000/books',{
-          name: this.form.name,
-          }, config)
+          axios.get('http://localhost:5000/authors', config)
+          .then(res => (this.authors = res.data))
           .catch(err => {
           console.log(err);
           })
-        console.log('es2')
       }
-    
     }
   }
 </script>
